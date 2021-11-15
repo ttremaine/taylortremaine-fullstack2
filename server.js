d1f243b41b45b139416e4598156e3c3b7887a9b7
Taylor Tremaine <76922507+ttremaine@users.noreply.github.com>
2021-11-15 10:15:34 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,119 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const path = require("path");
+const dal     = require('./dal.js');
+const e = require('express');
+
+
+
+// used to serve static files from public directory
+app.use(express.static('client/build)'));
+app.use(cors());
+//app.use(require('./routes/users'));
+
+//const path = require('path');
+//app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+// create user account
+app.get('/account/create/:name/:email/:password', function (req, res) {
+
+    // check if account exists
+    dal.find(req.params.email).
+        then((users) => {
+
+            // if user exists, return error message
+            if(users.length > 0){
+                console.log('User already exists');
+                res.send('User already exists');    
+            }
+            else{
+                // else create user
+                dal.create(req.params.name,req.params.email,req.params.password).
+                    then((user) => {
+                        console.log(user);
+                        res.send(user);            
+                    });            
+            }
+
+        });
+});
+
+
+// login user 
+app.get('/account/login/:email/:password', function (req, res) {
+
+    dal.find(req.params.email).
+        then((user) => {
+
+            // if user exists, check password
+            if(user.length > 0){
+                if (user[0].password === req.params.password){
+                    res.send(user[0]);
+                }
+                else{
+                    res.send('Login failed: wrong password');
+                }
+            }
+            else{
+                res.send('Login failed: user not found');
+            }
+    });
+    
+});
+
+// find user account
+app.get('/account/find/:email', function (req, res) {
+
+    dal.find(req.params.email).
+        then((user) => {
+            console.log(user);
+            res.send(user);
+    });
+});
+
+// find one user by email - alternative to find
+app.get('/account/findOne/:email', function (req, res) {
+
+    dal.findOne(req.params.email).
+        then((user) => {
+            console.log(user);
+            res.send(user);
+    });
+});
+
+
+// update - deposit/withdraw amount
+app.get('/account/update/:email/:amount', function (req, res) {
+
+    var amount = Number(req.params.amount);
+
+    dal.update(req.params.email, amount).
+        then((response) => {
+            console.log(response);
+            res.send(response);
+    });    
+});
+
+// all accounts
+app.get('/account/all', function (req, res) {
+
+    dal.all().
+        then((docs) => {
+            console.log(docs);
+            res.send(docs);
+    });
+});
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+//port
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    dal.connectToServer(function (err) {
+        if (err) console.error(err);
+    })
+    console.log('Running on port: ' + port);
+});
